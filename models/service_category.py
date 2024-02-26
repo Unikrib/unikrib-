5e997b8f5bd3fff02bfc8bfe8ab377c0b7cb906d
Dennisco12 <akinwonjowodennisco@gmail.com>
2024-02-26 02:25:31 +0100

    Updated service_categories endpoint

--- models/service_category.py
+++ models/service_category.py
@@ -7,6 +7,7 @@ class ServiceCategory(BaseModel, Base):
     '''This defines the service categories table'''
     __tablename__ = 'service_categories'
     name = Column(String(128), nullable=False)
+    image_url = Column(String(128), default='')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
